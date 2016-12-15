76260d78271efd7707ac6f2c3e3c2c72989c4982
esteban123456789 <Em9b@noos.fr>
2016-12-15 16:53:33 +0100

    update database script to use the good database ;-)

--- data/FormationOSS.sql
+++ data/FormationOSS.sql
@@ -15,6 +15,12 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+DROP DATABASE IF EXISTS `FormationOSS`;
+CREATE DATABASE `FormationOSS` COLLATE 'utf8_unicode_ci';
+USE `FormationOSS`;
+
+
 --
 -- Table structure for table `planning`
 --
