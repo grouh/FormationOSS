7b0186f7697ba3ec278d9aa2f092682f70b6eee0
Sylvain Pamart <sylvain.pamart@smile.fr>
2016-12-15 15:56:14 +0100

    [FEAT] update menu

--- views/menuView.php
+++ views/menuView.php
@@ -1,9 +1,12 @@
 <div style="width: 800px; padding: 5px; border: 1px solid black;">
     <h2>OSS Planning Application</h2>
     <div>
-        <a href="#">Home</a>
+        <a href="?action=listPlanning">Home</a>
+        <a href="?action=createPlanning">Create Planning</a>
         <a href="?action=listUsers">Users</a>
+        <a href="?action=createUser">Create User</a>
         <a href="?action=listTrainers">Trainers</a>
+        <a href="?action=createTrainer">Create Trainer</a>
         <a href="#">Logout</a>
     </div>
 </div>
