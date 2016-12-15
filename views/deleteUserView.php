2a4360d385c59a85ff9492d1de036e6f04fc0d43
Dumbledoge <renaud.cavadaski@gmail.com>
2016-12-15 14:48:27 +0100

    [UPDATE] it is now possible to delete a user from the database.

--- views/deleteUserView.php
+++ views/deleteUserView.php
@@ -1,7 +1,5 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: formation
- * Date: 15/12/16
- * Time: 12:06
- */
+<form action="#" method="post">
+    <p>Do you want to delete the following user ? <?php echo $user['firstName'] . ' ' . $user['lastName']?> </p><br />
+    <input type="submit" value="Delete" name="delete"><br />
+    <input type="button" value="Cancel" name="cancel">
+</form>
