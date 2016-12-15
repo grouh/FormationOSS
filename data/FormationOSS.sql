01054c06ea6a69dc1fa0c6dad29f0ac15e3208e6
Dihcar <edjek@hotmail.fr>
2016-12-15 15:58:25 +0100

    nouvelle database

--- data/FormationOSS.sql
+++ data/FormationOSS.sql
@@ -1,70 +1,110 @@
--- MySQL dump 
+-- MySQL dump 10.13  Distrib 5.5.53, for debian-linux-gnu (x86_64)
 --
--- Host: 127.0.0.1    Database: FormationOSS
+-- Host: localhost    Database: FormationOSS
 -- ------------------------------------------------------
--- for Training project "PHP avancé"
+-- Server version	5.5.53-0ubuntu0.14.04.1
 
-
-DROP DATABASE IF EXISTS `FormationOSS`;
-CREATE DATABASE `FormationOSS` COLLATE 'utf8_unicode_ci';
-USE `FormationOSS`;
-
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `ID` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `login` varchar(256) COLLATE 'utf8_general_ci' NOT NULL,
-  `password` varchar(256) COLLATE 'utf8_general_ci' NOT NULL,
-  `role` varchar(256) COLLATE 'utf8_general_ci' NOT NULL,
-  `firstName` varchar(256) COLLATE 'utf8_general_ci' NULL,
-  `lastName` varchar(256) COLLATE 'utf8_general_ci' NULL,
-  `email` varchar(256) COLLATE 'utf8_general_ci' NULL,
-  `phone` varchar(256) COLLATE 'utf8_general_ci' NULL
-) COLLATE 'utf8_general_ci' AUTO_INCREMENT=1;
-
-
---
--- Dumping data for table `users`
---
--- ORDER BY:  `ID`
-
-INSERT INTO `users` (`ID`, `login`, `password`, `role`, `firstName`, `lastName`, `email`, `phone`) 
-VALUES
-(1,	'thlec',	'5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8',	'ADMIN',	'Thomas', 	'mdp=password',	NULL,	NULL),
-(2,	'marc',		'5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8',	'ADMIN',	'Marc',		'mdp=password',	NULL,	NULL),
-(3,	'user',		'12dea96fec20593566ab75692c9949596833adc9',	'USER',		'User',		'mdp=user',	NULL,	NULL);
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `planning`
 --
 
 DROP TABLE IF EXISTS `planning`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `planning` (
-  `ID` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `date` int(8) unsigned NOT NULL,
-  `label` varchar(256) COLLATE 'utf8_general_ci' NOT NULL,
-  `teach` varchar(256) COLLATE 'utf8_general_ci' NULL
-) COLLATE 'utf8_general_ci';
-
+  `label` varchar(256) NOT NULL,
+  `teach` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `planning`
 --
--- ORDER BY:  `ID`
 
-INSERT INTO `planning` (`ID`, `date`, `label`, `teach`) VALUES
-(1,	20161203,	'PHP base',	'Thomas'),
-(2,	20161205,	'PHP base',	'Thomas'),
-(3,	20161206,	'PHP base',	'Thomas'),
-(4,	20161207,	'PHP base',	'Thomas'),
-(5,	20161208,	'PHP base',	'Thomas'),
-(6,	20161209,	'PHP avancé',	'Sylvain'),
-(7,	20161211,	'PHP avancé',	'Sylvain'),
-(8,	20161212,	'PHP avancé',	'Sylvain'),
-(9,	20161213,	'PHP avancé',	'Sylvain'),
-(10,	20161214,	'PHP avancé',	'Sylvain');
+LOCK TABLES `planning` WRITE;
+/*!40000 ALTER TABLE `planning` DISABLE KEYS */;
+INSERT INTO `planning` VALUES (1,20161203,'PHP base',0),(2,20161205,'PHP base',0),(3,20161206,'PHP base',0),(4,20161207,'PHP base',0),(5,20161208,'PHP base',0),(6,20161209,'PHP avancé',0),(7,20161211,'PHP avancé',0),(8,20161212,'PHP avancé',0),(9,20161213,'PHP avancé',0),(10,20161214,'PHP avancé',0);
+/*!40000 ALTER TABLE `planning` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trainers`
+--
+
+DROP TABLE IF EXISTS `trainers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trainers` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(256) DEFAULT NULL,
+  `lastName` varchar(256) DEFAULT NULL,
+  `email` varchar(256) DEFAULT NULL,
+  `phone` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trainers`
+--
+
+LOCK TABLES `trainers` WRITE;
+/*!40000 ALTER TABLE `trainers` DISABLE KEYS */;
+INSERT INTO `trainers` VALUES (1,'Rachi','EDJEKOUANE','edjek@hotmail.fr','0658869337'),(5,'test','TEST','test@hotmail.fr','0658869337');
+/*!40000 ALTER TABLE `trainers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `role` varchar(256) NOT NULL,
+  `firstName` varchar(256) DEFAULT NULL,
+  `lastName` varchar(256) DEFAULT NULL,
+  `email` varchar(256) DEFAULT NULL,
+  `phone` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'thlec','5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8','ADMIN','Thomas','mdp=password',NULL,NULL),(3,'user','12dea96fec20593566ab75692c9949596833adc9','USER','User','mdp=user',NULL,NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-12-15 15:55:16
