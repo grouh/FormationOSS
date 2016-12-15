504f66849225de0c4e720523bf0f593e3ea91e1b
Mar_Des <deschamps.ma@gmail.com>
2016-12-15 09:50:14 +0100

    avancement create planning

--- manager/planningManager.php
+++ manager/planningManager.php
@@ -1,12 +1,34 @@
 <?php
 
-// createPlanning
-if (isset($_POST["createPlanning"])) {
-    if (isset($_POST["Date"]) && isset($_POST["Label"]) && isset($_POST["Teacher"])) {
-        $date = $_POST["Date"];
-        $label = $_POST["Label"];
-        $teacher = $_POST["Teacher"];
-       // createPLanning($date, $label, $teacher);
+
+/**
+ * Créer un nouveau planning
+ * @param date $date
+ * @param string $cours
+ * @param string $nameTeacher
+ */
+function createPlanning($date, $cours, $nameTeacher) {
+    echo "debut createPlanning DB ";
+    try {
+        $dsn = 'mysql:host=localhost;dbname=FormationOSS';
+        $user = 'root';
+        $pass = 'root';
+        $dbh = new PDO($dsn, $user, $pass);
+        $query = $dbh->prepare(""
+            . " INSERT INTO PLANNING "
+            . " VALUES ( " . $date . ", " . $cours . ", " . $nameTeacher . ");");
+        echo $query;
+
+        //$query->execute();
+
+
     }
+
+
+    catch(Exception $e) {
+        //excep
+        echo "base pas encore présente ";
+    }
+
 }
 
