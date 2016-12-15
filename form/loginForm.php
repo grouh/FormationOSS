d825d9bb6a9453cbfaa5e8157bd5383ebbd75dfe
mehdi <mehdi.mkadem@yahoo.com>
2016-12-15 11:45:25 +0100

    check for errors , check the  user, create sesssion

---
+++ form/loginForm.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function check_form($login, $password) {
+
+    if (isset($login) && isset($password)) {
+
+        return FALSE;
+    } else {
+
+
+        return 'login incorrect please try again or sign-up';
+    }
+
+    if (userIsOK($login, $password)) {
+
+        header("Loacation:../views/planningView.php");
+    }
+
+
+
+}
